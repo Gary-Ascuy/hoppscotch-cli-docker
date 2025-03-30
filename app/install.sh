7cda3eb706176337b3608850bfabf5a0718f8f49
Gary Ascuy <gary.ascuy@gmail.com>
2025-03-30 11:14:14 -0500

    Updating build scripts

--- app/install.sh
+++ app/install.sh
@@ -1,2 +1,2 @@
-LIB_VERSION=$(jq -r '.dependencies["@hoppscotch/cli"]' package.json)
-npm i -g @hoppscotch/cli@${LIB_VERSION}
+HOPPSCOTCH_CLI_VERSION=$(jq -r '.dependencies["@hoppscotch/cli"]' package.json)
+npm i -g @hoppscotch/cli@${HOPPSCOTCH_CLI_VERSION}
